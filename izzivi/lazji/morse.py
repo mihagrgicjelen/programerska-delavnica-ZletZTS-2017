a857ff3da6897f4413fa1ece369ada00311f1042
mihagrgicjelen <miha.grgic.jelen@gmail.com>
2017-08-04 16:00:22 +0200

    code style

--- izzivi/lazji/morse.py
+++ izzivi/lazji/morse.py
@@ -53,28 +53,29 @@ CODE_REVERSED={
     '----.': '9'
 }
 
+
 def text_v_morse(text):
     """Vnesi besedo (crke in stevilke) in pretvori v morse."""
     text = text.upper()
     morse = ""
 
     for znak in text:
-        morse +=CODE[znak] + "   "
+        morse += CODE[znak] + "   "
 
-    morse = morse[:-1]
+    morse = morse[:-1]  # zbrisi zadnji presledek!
     return morse
+
 
 def morse_v_text(morse):
     """Vnesi morse - crke locene s presledki - in pretvori v besedo"""
     seznam_morse = morse.split(" ")
-    seznam_text =[]
+    seznam_text = []
+
     for crka in seznam_morse:
         seznam_text.append(CODE_REVERSED[crka])
+
     text = "".join(seznam_text)
     return text
-    
-    
-
 
 
 print(text_v_morse('tralala'))
